d2795112026a5b361c6f77ce6de0f6186b760d07
waffle <jamesrmp4@gmail.com>
2026-06-29 22:54:57 +0800

    config: increase WaitingDurationSeconds to 30s and RoundIntervalMs to 3000ms

--- backend/src/CubeRacing.Application/Config/GameSettings.cs
+++ backend/src/CubeRacing.Application/Config/GameSettings.cs
@@ -5,9 +5,9 @@ public class GameSettings
     public int    MapLength              { get; init; } = 20;
     public int    NpcCount              { get; init; } = 4;
     public int    BettingDurationSeconds { get; init; } = 60;
-    public int    WaitingDurationSeconds { get; init; } = 5;
+    public int    WaitingDurationSeconds { get; init; } = 30;
     public int    RaceStartDelaySeconds  { get; init; } = 30;
-    public int    RoundIntervalMs        { get; init; } = 1500;
+    public int    RoundIntervalMs        { get; init; } = 3000;
     public int    InitialChips           { get; init; } = 1000;
     public double DefaultOdds            { get; init; } = 1.3;
 }
